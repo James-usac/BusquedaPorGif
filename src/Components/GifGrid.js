7f33d858195d0595343912ac99c5160580a5fe9b
James-usac <2811874520101@ingenieria.usac.edu.gt>
2021-11-05 20:31:53 -0600

    subiendo cambios a la app

--- src/Components/GifGrid.js
+++ src/Components/GifGrid.js
@@ -1,4 +1,6 @@
 
+import { ImageList, ImageListItem, Modal } from '@mui/material';
+import { Box } from '@mui/system';
 import React, { useEffect } from 'react'
 
 export const GifGrid = ({ category }) => {
@@ -6,6 +8,36 @@ export const GifGrid = ({ category }) => {
     const api_key = "CXlwDg9c0PUX5bdLjOUkBPfbZsmWSmAj";
 
     const [images, setImages] = React.useState([]);
+
+    const [open, setOpen] = React.useState(false);
+
+    const [selectedImg, setSelectedImg] = React.useState(null);
+
+    const [selecttitle, setSelecttitle] = React.useState('');
+
+    const handleOpen = (img, title) => {
+        setSelectedImg(img);
+        setSelecttitle(title);
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        backgroundColor: 'white',
+        pt: 2,
+        px: 4,
+        pb: 3,
+    };
 
     useEffect(() => {
         getGifs(category);
@@ -31,15 +63,34 @@ export const GifGrid = ({ category }) => {
     return (
         <div>
             <h1>{category}</h1>
-            {
-                images.map(img => (
-                    <div key={img.id}>
-                        <img src={img.url} alt={img.title} />
-                        <br/>
-                        <label>{img.title}</label>
-                    </div>
-                ))
-            }
+            <ImageList sx={{ maxWidth: 500 }} cols={3} rowHeight={164}>
+                {
+                    images.map(img => (
+                        <ImageListItem className="zoomImage" key={img.id}>
+                            <img 
+                                className="zoomImage" 
+                                onClick={() => handleOpen(img.url, img.title)}
+                                src={img.url} alt={img.title} />
+                        </ImageListItem>
+                    ))
+                }
+            </ImageList>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ ...style, width: 400 }}>
+                    <h2 id="parent-modal-title">{selecttitle}</h2>
+                    <img 
+                        src={selectedImg} 
+                        alt="gif"
+                        width="400"
+                        height="200" 
+                    />
+                </Box>
+            </Modal>
         </div>
     )
 }
--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { AddCategory } from './Components/AddCategory';
 import { GifGrid } from './Components/GifGrid';
+import { MenuFind } from './Components/MenuFind';
 
 export const GifExpertApp = () => {
 
@@ -8,14 +8,14 @@ export const GifExpertApp = () => {
 
     return (
         <div>
-            <h2>GifExpertApp</h2>
-            <AddCategory setCategories={ setCategories }></AddCategory>
-            <hr />
+            <MenuFind setCategories={ setCategories }></MenuFind>
+            <div className="center_body">
             {
                 categories.map((category) => (
                         <GifGrid category={category} key={category}></GifGrid>               
                 ))
             }
+            </div>
         </div>
     )
 }
